160046b6d2ef6e3f2e9928efd29bb01eb334d672
Domenico Tenace <domenicotenace99@gmail.com>
2023-05-02 16:14:57 +0200

    initial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["@vite-pwa/nuxt", "@pinia/nuxt", "nuxt-vitest"],
+  pwa: {
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+    manifest: {
+      name: "Micro-Commerce",
+      short_name: "Micro-Commerce",
+      description: "Everything you are looking for!✨",
+    },
+    workbox: {
+      navigateFallback: "/",
+    },
+    devOptions:{
+      enabled: true,
+      type: "module",
+    }
+  },
+});
