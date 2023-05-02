786c337229ada3cde6db0d404239300afc4eaced
Domenico Tenace <domenicotenace99@gmail.com>
2023-05-02 16:44:46 +0200

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const dynamicTitle = computed(() => useUpperCase(useRoute().name?.toString()!));
+</script>
+
+<template>
+  <VitePwaManifest />
+  <Head>
+    <Title
+      >Micro Commerce -
+      {{ dynamicTitle === "Index" ? "Home" : dynamicTitle }}</Title
+    >
+    <Navbar />
+    <section>
+      <slot></slot>
+    </section>
+    <Footer />
+  </Head>
+</template>
