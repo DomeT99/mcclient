1fc94a22aacedaf9df5e9c3510a24e903ab5ac4c
Domenico Tenace <domenicotenace99@gmail.com>
2023-05-10 15:42:15 +0200

    feat: newsletter section complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { EButton, EText } from "easy-kit-component";
+</script>
+
 <template>
   <section>
     <HomeHero></HomeHero>
-    <section class="container mt-6">
+
+    <section class="container min-h-50 mt-6 mb-6">
       <HomeTopBrand></HomeTopBrand>
+    </section>
+
+    <section class="mt-6 bg-tertiary-dark">
+      <div class="has-text-centered pt-2">
+        <h2>Subscribe to the newsletter</h2>
+      </div>
+      <div class="mt-6 min-h-25">
+        <Form>
+          <div class="columns is-vcentered">
+            <div
+              class="column is-5-desktop is-offset-3-desktop is-12-mobile is-9-tablet"
+            >
+              <EText
+                :place-holder="'Enter your email'"
+                class="input is-primary is-medium"
+              />
+            </div>
+            <div class="column is-2-desktop is-12-mobile is-3-tablet">
+              <EButton class="button is-secondary is-medium w-100"
+                ><strong>Subscribe</strong></EButton
+              >
+            </div>
+          </div>
+        </Form>
+      </div>
     </section>
   </section>
 </template>
