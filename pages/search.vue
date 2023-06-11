5ccaf6726722869476da59d056bdaaea0c13ed80
Dvalin <domenicotenace99@gmail.com>
2023-06-11 12:21:36 +0200

    feat: search bar at search section

--- pages/search.vue
+++ pages/search.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { EText, EButton } from "easy-kit-component";
+</script>
+
 <template>
-  <div></div>
+  <section class="mt-6">
+    <div class="columns">
+      <div class="column is-7-desktop is-offset-2-desktop is-10-tablet">
+        <EText class="input is-medium is-primary" />
+      </div>
+      <div class="column is-2-desktop">
+        <EButton class="button is-primary is-medium w-100">Search</EButton>
+      </div>
+    </div>
+  </section>
 </template>
